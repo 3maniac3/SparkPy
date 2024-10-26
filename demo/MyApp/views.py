60759dda85ec66736df739a75b63177fd83a10db
GeoCosmic <157273849+3maniac3@users.noreply.github.com>
2024-10-26 19:27:07 +0700

    Add files via upload

---
+++ demo/MyApp/views.py
@@ -0,0 +1,10 @@
+import os
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Web views
+def index(response):
+	return HttpResponse("<h1>Hello, World!</h1>")
+	
+def user(response):
+	return render(response, "main/base.html", {"content": os.getcwd()})
