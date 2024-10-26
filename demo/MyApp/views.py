aea301bfadc56232f2085ed9d41a4849fcb561b9
GeoCosmic <157273849+3maniac3@users.noreply.github.com>
2024-10-26 22:57:01 +0700

    Update views.py

--- demo/MyApp/views.py
+++ demo/MyApp/views.py
@@ -6,4 +6,4 @@ def index(response):
 	return HttpResponse("<h1>Hello, World!</h1>")
 	
 def user(response):
-	return render(response, "main/base.html", {"content": "Hello there."})
+	return render(response, "main/base.html", {"content": "Goodbye World."})
